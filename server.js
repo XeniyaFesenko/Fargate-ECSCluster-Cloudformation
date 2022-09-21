57649f3788f756a97ba5e5dd5efe8d7c11680953
XeniyaFesenko <48219240+XeniyaFesenko@users.noreply.github.com>
2022-09-21 11:10:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World! Test for ECSFargate Cluster');
+  res.send('Hello World! testing new push for ECS Fargate ');
 });
 
 app.listen(PORT, HOST);

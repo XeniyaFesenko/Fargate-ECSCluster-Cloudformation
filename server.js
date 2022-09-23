e5b8aa08dead2c6552b53df9352d570967c97408
XeniyaFesenko <48219240+XeniyaFesenko@users.noreply.github.com>
2022-09-23 13:11:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World! testing new push for ECS Fargate Prod Deployment ');
+  res.send('Hello World! testing new push for testing new push for Prod env ');
 });
 
 app.listen(PORT, HOST);
